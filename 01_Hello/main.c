8356af0df0434d6de2ad31e6c56ea1b76a1c0a5b
Mike Chen <mikecpp.chen@msa.hine.tnet>
2018-09-17 13:43:09 +0800

    update

--- 01_Hello/main.c
+++ 01_Hello/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "fsl_device_registers.h"
 #include "fsl_debug_console.h"
 #include "board.h"
@@ -5,19 +6,37 @@
 #include "pin_mux.h"
 #include "clock_config.h"
 
+volatile uint32_t g_systickCounter;
+
+void SysTick_Handler(void)
+{
+    if (g_systickCounter != 0U) { 
+        g_systickCounter--;
+    }
+}
+
+void delay_ms(uint32_t n)
+{
+    g_systickCounter = n;
+    
+    while(g_systickCounter != 0U) {
+    }
+}
+
 int main(void)
 {
-    char ch;
-
-    BOARD_InitPins();
+    BOARD_InitPins();   
     BOARD_BootClockRUN();
+
+    SysTick_Config(SystemCoreClock / 1000U);
     BOARD_InitDebugConsole();
 
-    PRINTF("Hello World.\r\n");
+    PRINTF("Hello World! \r\n");
+    printf("Hello again! \r\n");
 
-    while (1)
-    {
-        ch = GETCHAR();
-        PUTCHAR(ch);
+    int count = 0;
+    while (1){
+        PRINTF("Count = %d \r\n", count++);
+        delay_ms(1000);  
     }
 }
